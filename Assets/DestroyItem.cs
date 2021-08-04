08d73a8e5f11e17a882b0904b8418df86d809b71
curln <87195095+curln@users.noreply.github.com>
2021-08-04 16:43:07 +0900

    OnBecameInvisible()を使わずアイテムを消す

--- Assets/DestroyItem.cs
+++ Assets/DestroyItem.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class DestroyItem : MonoBehaviour
 {
+     GameObject unityChan;
     // Start is called before the first frame update
     void Start()
     {
-        
+        unityChan = GameObject.Find("unitychan");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (unityChan.transform.position.z - 5 > transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
     }
-    void OnBecameInvisible()
-    {
-
-        Destroy(this.gameObject);
-    }
+   
 }
